9a61a7cb582dd7d347aa1fe6a53947e35ee0a61c
KateM3d <ekaterina8medvedeva@gmail.com>
2022-04-09 20:18:45 -0400

    added categories

--- src/components/filter/AllCategories.jsx
+++ src/components/filter/AllCategories.jsx
@@ -1,5 +1,14 @@
+import Filter from "./Filter";
+
 const AllCategories = () => {
-  return <h2>AllCategories</h2>;
+  return (
+    <>
+      <h2>What kind of food do you like?</h2>
+      {["SEAFOOD", "ITALIAN", "APPETIZERS", "SALADS", "ALL"].map((category) => (
+        <Filter category={category} />
+      ))}
+    </>
+  );
 };
 
 export default AllCategories;
