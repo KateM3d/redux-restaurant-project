e44307e8c5ce770c7005aefe4d05288c777a2180
KateM3d <ekaterina8medvedeva@gmail.com>
2022-04-09 20:18:21 -0400

    added buttons

--- src/components/dishesComponents/Dish.jsx
+++ src/components/dishesComponents/Dish.jsx
@@ -4,6 +4,9 @@ const Dish = ({ id, img, name, price }) => {
       <img src={`${img}.jpg`} alt="img" />
       <h2>{name}</h2>
       <p>${price}</p>
+      {/*to fix later*/}
+      <button>How many portions??</button>
+      <button>Add to cart</button>
     </div>
   );
 };
