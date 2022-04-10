38d2df152e51c78e73cfd8c3442e0ce6f860ed28
KateM3d <ekaterina8medvedeva@gmail.com>
2022-04-09 20:18:04 -0400

    added Cart icon

--- src/components/cart/Cart.jsx
+++ src/components/cart/Cart.jsx
@@ -1,5 +1,13 @@
 const Cart = () => {
-  return <h2>Cart</h2>;
+  return (
+    <>
+      <img
+        className="cartIcon"
+        src="https://img.icons8.com/external-xnimrodx-lineal-color-xnimrodx/64/000000/external-shopping-cart-cyber-monday-xnimrodx-lineal-color-xnimrodx.png"
+        alt="cart"
+      />
+    </>
+  );
 };
 
 export default Cart;
