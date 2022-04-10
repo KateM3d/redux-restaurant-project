0ad08cc1ea640309a356fb2cef2ba639ae44d07b
KateM3d <ekaterina8medvedeva@gmail.com>
2022-04-09 21:12:42 -0400

    added store

--- src/components/filter/Filter.jsx
+++ src/components/filter/Filter.jsx
@@ -1,7 +1,19 @@
+import { useSelector } from "react-redux";
+import { getSelectedCategory } from "../../redux/dishesSlice";
+
 const Filter = ({ category }) => {
+  const selectedCategory = useSelector(getSelectedCategory);
   return (
     <div>
-      <p>{category}</p>
+      <p
+        className={
+          selectedCategory === category
+            ? "categoryButtonSelected"
+            : "categoryButton"
+        }
+      >
+        {category}
+      </p>
     </div>
   );
 };
